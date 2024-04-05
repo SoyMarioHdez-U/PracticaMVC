4c61bedfa2d13bca2dda92d6a27a6e00e3ceb316
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-04-04 19:33:47 -0600

    Se agregó lo de marcas

--- PracticaMVC/Models/marcas.cs
+++ PracticaMVC/Models/marcas.cs
@@ -5,8 +5,13 @@ namespace PracticaMVC.Models
     public class marcas
     {
         [Key]
+        [Display(Name = "Id")]
         public int id_marcas { get; set; }
+
+        [Display(Name = "Marca")]
         public string? nombre_marca { get; set; }
+
+        [Display(Name = "Estado")]
         public string? estados {  get; set; }
     }
 }
